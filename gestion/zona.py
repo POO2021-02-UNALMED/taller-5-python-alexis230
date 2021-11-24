e9b3bdcf23cf1e96d6a35425b41aa00c8c4a8bca
alexis <alexgarca31@gmail.com>
2021-11-23 22:03:45 -0500

    commit1

--- gestion/zona.py
+++ gestion/zona.py
@@ -1,13 +1,32 @@
 class Zona:
 
-  def __init__(self, nombre, zoo, animales):
-     self._nombre = nombre
-     self._zoo = zoo
-     self._animales = animales
-    
-  def agregarAnimales():
-      return ""
+    def __init__(self, nombre = None , zoo = None):
+        self.setNombre(nombre)
+        self.setZoo(zoo)
+        self._animales = []
 
-  def cantidadAnimales():
-      return ""
+    def agregarAnimales(self, animal):
+        self.getAnimales().append(animal)
+        animal.setZona(self)
+
+    def cantidadAnimales(self):
+        return len(self.getAnimales())
+
+    def getNombre(self):
+        return self._nombre
+
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getZoo(self):
+        return self._zoo
+
+    def setZoo(self, zoo):
+        self._zoo = zoo
+
+    def getAnimales(self):
+        return self._animales
+
+    def setAnimales(self, animales):
+        _animales = animales
          
--- gestion/zoologico.py
+++ gestion/zoologico.py
@@ -1,13 +1,41 @@
 class Zoologico:
 
-  def __init__(self, nombre, ubicacion, zonas):
-     self._nombre = nombre
-     self._ubicacion = ubicacion
-     self._zonas = zonas
-    
-  def agregarZonas():
-      return ""
+    _zonas = []
+    def __init__(self, nombre = None, ubicacion = None):
+        self.setNombre(nombre)
+        self.setUbicacion(ubicacion)
 
-  def cantidadTotalAnimales():
-      return ""
+    def agregarZonas(self, zona):
+        self.getZona().append(zona)
+
+    def cantidadTotalAnimales(self):
+        numAnimales = 0
+        for i in range (len(self.getZona())):
+            zonaActual = self.getZona()[i]
+            animales = zonaActual.getAnimales()
+            numAnimales += len(animales)
+        return numAnimales
+
+    def getNombre(self):
+        return self._nombre
+
+
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+
+    def getUbicacion(self):
+        return self._ubicacion
+
+
+    def setUbicacion(self, ubicacion):
+        self._ubicacion = ubicacion
+
+
+    def getZona(self):
+        return Zoologico._zonas
+
+
+    def setZonas(self, zonas):
+        Zoologico._zonas = zonas
          
